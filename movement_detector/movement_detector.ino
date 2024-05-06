b8acb332537f0f8d59cb65a3cab24649ecda6f86
LTames <tamess@protonmail.com>
2024-05-06 20:24:14 -0300

    feat: add comments

--- movement_detector/movement_detector.ino
+++ movement_detector/movement_detector.ino
@@ -1,25 +1,20 @@
-int pirPin = D2;
-int val;
 
-void setup()
-{
-Serial.begin(9600);
+int pirPin = D2; // Define o pino conectado ao sensor PIR.
+int val; // Armazena o estado lido pelo sensor.
+
+void setup() {
+  Serial.begin(9600); 
 }
 
-void loop()
-{
-val = digitalRead(pirPin);
-//low = no motion, high = motion
-Serial.println(val);
+void loop() {
+  val = digitalRead(pirPin); // Lê o estado do sensor PIR.
 
-if (val == LOW)
-{
-  Serial.println("No motion");
-}
-else
-{
-  Serial.println("Motion detected  ALARM");
+  if (val == LOW) {
+    Serial.println("No motion"); // Se não houver movimento, printa "No motion".
+  } else {
+    Serial.println("Motion detected  ALARM"); // Se detectar movimento, printa "Motion detected ALARM".
+  }
+
+  delay(100); // Aguarda 100 milissegundos antes de repetir.
 }
 
-delay(10);
-}
